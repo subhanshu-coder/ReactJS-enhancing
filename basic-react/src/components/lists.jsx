d165387f6e79104e5d7d522790fcbf64f852aa2e
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-28 00:00:29 +0530

    update all

--- basic-react/src/components/lists.jsx
+++ basic-react/src/components/lists.jsx
@@ -13,3 +13,23 @@ function MyCars() {
 createRoot(document.getElementById('root')).render(
   <MyCars />
 );
+
+function MyCars() {
+  const cars = [
+    {id: 1001, brand: 'Ford'},
+    {id: 1002, brand: 'BMW'},
+    {id: 1003, brand: 'Audi'}
+  ];
+  return (
+    <>
+      <h1>My Cars:</h1>
+      <ul>
+        {cars.map((car) => <li key={car.id}>I am a { car.brand }</li>)}
+      </ul>
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <MyCars />
+);
