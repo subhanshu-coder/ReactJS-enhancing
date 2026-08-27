6ae6ed622c2585d62253399f3d65502feeb740af
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-27 23:59:37 +0530

    update all

---
+++ basic-react/src/components/lists.jsx
@@ -0,0 +1,9 @@
+function MyCars() {
+  const cars = ['Ford', 'BMW', 'Audi'];
+  return (
+    <>
+      <h1>My Cars:</h1>
+      <ul>
+        {cars.map((car) => <li>I am a { car }</li>)}
+      </ul>
+    </>
