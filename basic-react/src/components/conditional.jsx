79284a0047354f232b952ad24ca99065bb4542b9
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-27 23:46:59 +0530

    update all

--- basic-react/src/components/conditional.jsx
+++ basic-react/src/components/conditional.jsx
@@ -1,3 +1,6 @@
 // In React, you can conditionally render components.
 
 // There are several ways to do this.
+// Example:
+// Now, we'll create another component that chooses which component to render based on a condition:
+
