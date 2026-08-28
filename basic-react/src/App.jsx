887f9e9edbc6ef2ae0e571c0116cae1753884246
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-28 23:10:32 +0530

    update all

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -3,7 +3,7 @@ function App () {
   return (
     <div>
       <h1>Hello React</h1>
-      <UserCard name="subhanshu pal" />
+      <UserCard name="subhanshu pal"  age={30}/>
       <UserCard name="prince pal" />
       <UserCard name="aditya singh"/>
     </div>
