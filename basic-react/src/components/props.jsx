c7e029e7f4dd593fa04e0865d76585181f44bbbd
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-28 23:52:20 +0530

    update all

--- basic-react/src/components/props.jsx
+++ basic-react/src/components/props.jsx
@@ -42,14 +42,14 @@ createRoot(document.getElementById('root')).render(
 );
 
 // // Use the dot notation to access object properties:
-// function Car(props) {
-//   return (
-//     <>
-//       <h2>My {props.carinfo.name} {props.carinfo.model}!</h2>
-//       <p>It is {props.carinfo.color} and it is from {props.carinfo.year}!</p>
-//     </>
-//   );
-// }
+function Car(props) {
+  return (
+    <>
+      <h2>My {props.carinfo.name} {props.carinfo.model}!</h2>
+      <p>It is {props.carinfo.color} and it is from {props.carinfo.year}!</p>
+    </>
+  );
+}
 
 // const carInfo = {
 //   name: "Ford",
