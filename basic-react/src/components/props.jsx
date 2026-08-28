97367b1f6af9dbcf3ca98c13ae32e565d634d355
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-28 23:51:06 +0530

    update all

--- basic-react/src/components/props.jsx
+++ basic-react/src/components/props.jsx
@@ -20,11 +20,11 @@ createRoot(document.getElementById('root')).render(
 );
 
 // // use the property values in the car component
-// function Car(props) {
-//   return (
-//     <h2>I am a {props.color} {props.brand} {props.model}!</h2>
-//   );
-// }
+function Car(props) {
+  return (
+    <h2>I am a {props.color} {props.brand} {props.model}!</h2>
+  );
+}
 
 // // variables has to be sent inside curly brackets
 // // let x = "Ford";
