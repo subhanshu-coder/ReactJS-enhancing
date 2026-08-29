bd3ec80b1f7dab13c223e52f3ab74b31db3437cf
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-29 23:41:11 +0530

    update all

--- basic-react/src/components/forms.jsx
+++ basic-react/src/components/forms.jsx
@@ -22,3 +22,16 @@ function MyForm() {
   function handleChange(e) {
     setName(e.target.value);
   }
+    return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <p>Current value: {name}</p>
+    </form>
+  )
+}
