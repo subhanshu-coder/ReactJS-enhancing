61556e015b750c83b9de7ba8ae99fcec83ee66ce
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-29 23:42:55 +0530

    update all

--- basic-react/src/components/forms.jsx
+++ basic-react/src/components/forms.jsx
@@ -38,3 +38,22 @@ function MyForm() {
 createRoot(document.getElementById('root')).render(
   <MyForm />
 );
+
+// Submitting Forms
+// You can control the submit action by adding an event handler in the onSubmit attribute for the <form>:
+// add a submit button and an event handler in the onSubmit attribute:
+
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(name);
+  }
