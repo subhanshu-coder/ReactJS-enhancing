98c6e0a9b8a00707f3440986927f13246c3f7917
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-30 23:06:27 +0530

    update all

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,5 +1,5 @@
-import UserCard from "./components/props"
-// import Props from index.css
+
+import React from 'react'
 import Card from './components/Card'
 
 function App () {
--- basic-react/src/components/Card.jsx
+++ basic-react/src/components/Card.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 function Card () {
 
     return (
@@ -9,3 +10,4 @@ function Card () {
         </div>
     )
 }
+export default Card
