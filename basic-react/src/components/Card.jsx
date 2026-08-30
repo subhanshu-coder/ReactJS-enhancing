2851819a6707d2914f55aa904f3f673d4b7091b8
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-30 22:55:32 +0530

    update all

---
+++ basic-react/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card () {
+
+    return (
+        <div className ='child'>
+          <img src= "https://unsplash.com/photos/a-holographic-dome-with-circular-openings-dPzF4JbtxC8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"></img>
+          <h1>prince pal</h1>
+          <p>just sense the human behaviour how they belong to dumb society</p>
+          <button>check the profile</button>
+        </div>
+    )
+}
