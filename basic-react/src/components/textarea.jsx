f2b09f8976947c6ba5fef317f40c45e8f38a65aa
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-31 23:39:18 +0530

    update aall

--- basic-react/src/components/textarea.jsx
+++ basic-react/src/components/textarea.jsx
@@ -15,17 +15,17 @@
 // function MyForm() {
 //   const [mytxt, setMytxt] = useState("");
 
-//   function handleChange(e) {
-//     setMytxt(e.target.value);
-//   }
+  function handleChange(e) {
+    setMytxt(e.target.value);
+  }
   
-  // return (
-  //   <form>
-  //     <label>Write here:
-  //       <textarea
-  //         value={mytxt}
-  //         onChange={handleChange}
-  //       />
+  return (
+    <form>
+      <label>Write here:
+        <textarea
+          value={mytxt}
+          onChange={handleChange}
+        />
       </label>
       <p>Current value: {mytxt}</p>
     </form>
