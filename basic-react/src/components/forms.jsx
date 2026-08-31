6eb57bedbf5b72579dd1796b28c1d026581e817b
Subhanshu pal <subhanshupal7@gmail.com>
2026-08-31 23:28:25 +0530

    update all

--- basic-react/src/components/forms.jsx
+++ basic-react/src/components/forms.jsx
@@ -13,16 +13,16 @@ createRoot(document.getElementById('root')).render(
 
 // Example:
 // Use the useState Hook to manage the input:
-import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
+// import { useState } from 'react';
+// import { createRoot } from 'react-dom/client';
 
-function MyForm() {
-  const [name, setName] = useState("");
+// function MyForm() {
+//   const [name, setName] = useState("");
 
-  function handleChange(e) {
-    setName(e.target.value);
-  }
-    return (
+//   function handleChange(e) {
+//     setName(e.target.value);
+//   }
+//     return (
     // <form>
     //   <label>Enter your name:
     //     <input
