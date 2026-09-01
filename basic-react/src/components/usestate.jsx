84485f650b31dd11719aa3294132b42ee1791a4b
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:59:17 +0530

    update all

--- basic-react/src/components/usestate.jsx
+++ basic-react/src/components/usestate.jsx
@@ -55,3 +55,27 @@ createRoot(document.getElementById('root')).render(
   <MyCar />
 );
 
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function MyCar() {
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
+
+  return (
+    <>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+    </>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <MyCar />
+);
