67b906ed8268fe5b32768abec2ffff00141a2f9d
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:20:02 +0530

    update all

--- basic-react/src/components/lists.jsx
+++ basic-react/src/components/lists.jsx
@@ -14,12 +14,12 @@
 //   <MyCars />
 // );
 
-// function MyCars() {
-//   const cars = [
-//     {id: 1001, brand: 'Ford'},
-//     {id: 1002, brand: 'BMW'},
-//     {id: 1003, brand: 'Audi'}
-//   ];
+function MyCars() {
+  const cars = [
+    {id: 1001, brand: 'Ford'},
+    {id: 1002, brand: 'BMW'},
+    {id: 1003, brand: 'Audi'}
+  ];
   return (
     <>
       <h1>My Cars:</h1>
