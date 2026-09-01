8382011db7633989608dc450d9ef227ebf6dacb0
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:41:59 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -5,3 +5,16 @@ function App() {
     </BrowserRouter>
   );
 }
+
+//example of react router
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
