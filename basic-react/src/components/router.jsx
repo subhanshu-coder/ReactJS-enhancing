ba0ce890717accda012520b9899af46e9a38d752
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:49:00 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -74,3 +74,16 @@ function CarProducts() {
     </div>
   );
 }
+
+function BikeProducts() {
+  return (
+    <div>
+      <h2>Bikes</h2>
+      <ul>
+        <li>Yamaha</li>
+        <li>Suzuki</li>
+        <li>Honda</li>
+      </ul>
+    </div>
+  );
+}
