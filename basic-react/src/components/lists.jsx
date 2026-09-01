3d32280d0c9d7ea58e8370a8873579725821aefe
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:19:16 +0530

    update all

--- basic-react/src/components/lists.jsx
+++ basic-react/src/components/lists.jsx
@@ -20,10 +20,10 @@
 //     {id: 1002, brand: 'BMW'},
 //     {id: 1003, brand: 'Audi'}
 //   ];
-//   return (
-//     <>
-//       <h1>My Cars:</h1>
-//       <ul>
+  return (
+    <>
+      <h1>My Cars:</h1>
+      <ul>
         {cars.map((car) => <li key={car.id}>I am a { car.brand }</li>)}
       </ul>
     </>
