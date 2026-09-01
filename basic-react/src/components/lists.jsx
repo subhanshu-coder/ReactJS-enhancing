e87658ef57094eb861347babecdd94945ca948af
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:21:15 +0530

    update all

--- basic-react/src/components/lists.jsx
+++ basic-react/src/components/lists.jsx
@@ -4,11 +4,11 @@
 //     <>
 //       <h1>My Cars:</h1>
 //       <ul>
-//         {cars.map((car) => <li>I am a { car }</li>)}
-//       </ul>
-//     </>
-//       );
-// }
+        {cars.map((car) => <li>I am a { car }</li>)}
+      </ul>
+    </>
+      );
+}
 
 createRoot(document.getElementById('root')).render(
   <MyCars />
