f8257ef29c10c905dfacb03d0a602614366f8906
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:44:50 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -29,3 +29,14 @@ function App() {
         <Link to="/about">About</Link> |{" "}
         <Link to="/contact">Contact</Link>
       </nav>
+
+      
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
