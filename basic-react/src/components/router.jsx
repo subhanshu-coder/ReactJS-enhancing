fffb2396631d06408aae2e3d06f6c28cc41fc21a
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:46:55 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -40,3 +40,12 @@ function App() {
     </BrowserRouter>
   );
 }
+ 
+
+// new one
+
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
