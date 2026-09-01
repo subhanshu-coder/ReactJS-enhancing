e308523ac68186ed7e54dee6c61994b616ff1858
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 23:56:06 +0530

    update all

--- basic-react/src/components/usestate.jsx
+++ basic-react/src/components/usestate.jsx
@@ -3,4 +3,10 @@ import { useState } from "react";
 
 function FavoriteColor() {
   const [color, setColor] = useState("red");
+
+  return <h1>My favorite color is {color}!</h1>
 }
+
+createRoot(document.getElementById('root')).render(
+  <FavoriteColor />
+)
