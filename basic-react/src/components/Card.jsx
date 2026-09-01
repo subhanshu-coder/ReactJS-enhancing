9156236a1998cdae5a2c938f0f8ce1ea86173761
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-01 11:10:28 +0530

    update all

--- basic-react/src/components/Card.jsx
+++ basic-react/src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
     <div className="child">
       <img
         src="https://images.unsplash.com/photo-1752350434967-29fe9a749b37?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D"
-        alt="Holographic dome"
+        alt=""
       />
 
       <h1>{props.user}</h1>
