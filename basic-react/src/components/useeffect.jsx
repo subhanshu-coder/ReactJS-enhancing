1ec5665c9d50907e4d455807f5045f0a3e61a0c0
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-02 23:04:30 +0530

    update all

--- basic-react/src/components/useeffect.jsx
+++ basic-react/src/components/useeffect.jsx
@@ -17,12 +17,6 @@ createRoot(document.getElementById('root')).render(
   <Timer />
 );
 
-import { useState, useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-
-function Timer() {
-  const [count, setCount] = useState(0);
-
 
 
 
