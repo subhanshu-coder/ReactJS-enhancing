1802a12a5020e73e889b9270558b9e33509ff882
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-02 23:26:07 +0530

    update all

--- basic-react/src/components/useref.jsx
+++ basic-react/src/components/useref.jsx
@@ -57,3 +57,21 @@ function App() {
   useEffect(() => {
     previousInputValue.current = inputValue;
   }, [inputValue]);
+
+    return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h2>Current Value: {inputValue}</h2>
+      <h2>Previous Value: {previousInputValue.current}</h2>
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <App />
+);
+
