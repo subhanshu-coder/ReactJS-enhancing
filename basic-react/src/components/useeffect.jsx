1a3f7c1cd3e87645866a1d8d3672334ade3abec2
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-02 23:10:09 +0530

    update all

--- basic-react/src/components/useeffect.jsx
+++ basic-react/src/components/useeffect.jsx
@@ -36,3 +36,13 @@ createRoot(document.getElementById('root')).render(
   <Timer />
 );
 
+import { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]); // <- add the count variable here
