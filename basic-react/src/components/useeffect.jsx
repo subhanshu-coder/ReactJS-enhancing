25a78550b8e4603f979c2be7b339fd6b799c9f6c
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-02 23:10:38 +0530

    update all

--- basic-react/src/components/useeffect.jsx
+++ basic-react/src/components/useeffect.jsx
@@ -46,3 +46,16 @@ function Counter() {
   useEffect(() => {
     setCalculation(() => count * 2);
   }, [count]); // <- add the count variable here
+
+    return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <Counter />
+);
