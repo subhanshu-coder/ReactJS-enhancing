10daca8b966def3211865853683d1ba3fb34975c
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-02 23:16:44 +0530

    update all

--- basic-react/src/components/usecontext.jsx
+++ basic-react/src/components/usecontext.jsx
@@ -55,3 +55,19 @@ function Component3() {
     </>
   );
 }
+
+import { useState, createContext, useContext } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const UserContext = createContext();
+
+function Component1() {
+  const [user, setUser] = useState("Linus");
+
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 />
+    </UserContext.Provider>
+  );
+}
