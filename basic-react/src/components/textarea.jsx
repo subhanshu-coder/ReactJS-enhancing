95479a48289e737e65509116694f497276c58212
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:47:31 +0530

    update all

--- basic-react/src/components/textarea.jsx
+++
@@ -1,8 +0,0 @@
-// Textarea
-// The textarea element in React is slightly different from ordinary HTML.
-
-// In HTML the value of a textarea is the text between the start tag <textarea> and the end tag </textarea>.
-{ <textarea>
-  Content of the textarea.
-</textarea> }
-
