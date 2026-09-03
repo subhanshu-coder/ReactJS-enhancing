7175e43967980098e91863d2c52572dff95cd72c
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:34:57 +0530

    update all

--- basic-react/src/components/useref.jsx
+++ basic-react/src/components/useref.jsx
@@ -21,19 +21,6 @@ function App() {
   );
 }
 
-createRoot(document.getElementById('root')).render(
-  <App />
-);
-
-import { useRef } from 'react';
-import { createRoot } from 'react-dom/client';
-
-function App() {
-  const inputElement = useRef();
-
-  const focusInput = () => {
-    inputElement.current.focus();
-  };
 
 
 
