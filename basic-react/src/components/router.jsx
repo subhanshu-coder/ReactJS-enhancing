a080c28a2ae41910ddf6a3cf359e3166ac393066
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:53:51 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -136,22 +136,3 @@ function Contact() {
   return <h1>Contact Page</h1>;
 }
 
-function App() {
-  return (
-    <BrowserRouter>
-      {/* Navigation with NavLink for active styling */}
-      <nav style={{ marginBottom: '20px' }}>
-        <NavLink to="/" style={navLinkStyles}>Home</NavLink> |{" "}
-        <NavLink to="/about" style={navLinkStyles}>About</NavLink> |{" "}
-        <NavLink to="/contact" style={navLinkStyles}>Contact</NavLink>
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
