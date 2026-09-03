d91d74edfd7614ba06e2c11815e0f022559285e2
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:49:45 +0530

    update all

--- basic-react/src/components/usestate.jsx
+++ basic-react/src/components/usestate.jsx
@@ -11,23 +11,3 @@ createRoot(document.getElementById('root')).render(
   <FavoriteColor />
 )
 
-import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-
-function FavoriteColor() {
-  const [color, setColor] = useState("red");
-
-  return (
-    <>
-      <h1>My favorite color is {color}!</h1>
-            <button
-        type="button"
-        onClick={() => setColor("blue")}
-      >Blue</button>
-    </>
-  )
-}
-
-createRoot(document.getElementById('root')).render(
-  <FavoriteColor />
-);
