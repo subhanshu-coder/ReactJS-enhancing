214e552e7eb1737ac3d2660e1828c8ba1afc7481
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:49:05 +0530

    update all

--- basic-react/src/components/usestate.jsx
+++ basic-react/src/components/usestate.jsx
@@ -41,17 +41,3 @@ function MyCar() {
   const [year, setYear] = useState("1964");
   const [color, setColor] = useState("red");
 
-  return (
-    <>
-      <h1>My {brand}</h1>
-      <p>
-        It is a {color} {model} from {year}.
-      </p>
-    </>
-  )
-}
-
-createRoot(document.getElementById('root')).render(
-  <MyCar />
-);
-
