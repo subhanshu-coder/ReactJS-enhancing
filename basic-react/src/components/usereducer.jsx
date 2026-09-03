a79e23c36cc6f47e4d92ffeb0df84ce7edb4c812
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:38:05 +0530

    update all

--- basic-react/src/components/usereducer.jsx
+++ basic-react/src/components/usereducer.jsx
@@ -1,17 +1,2 @@
-import { useReducer } from 'react';
-import { createRoot } from 'react-dom/client';
-
-const initialScore = [
-  {
-    id: 1,
-    score: 0,
-    name: "John",
-  },
-  {
-    id: 2,
-    score: 0,
-    name: "Sally",
-  },
-];
 
 
