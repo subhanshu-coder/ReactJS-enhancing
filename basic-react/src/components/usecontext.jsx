a067ab69fc5b8a2cbd350d64957c7db8b80c63ed
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:44:19 +0530

    update all

--- basic-react/src/components/usecontext.jsx
+++ basic-react/src/components/usecontext.jsx
@@ -45,15 +45,4 @@ function Component1() {
   );
 }
 
-function Component3() {
-  const user = useContext(UserContext);
-
-  return (
-    <>
-      <h1>Component 3</h1>
-      <h2>{`Hello ${user} again!`}</h2>
-    </>
-  );
-}
-
 
