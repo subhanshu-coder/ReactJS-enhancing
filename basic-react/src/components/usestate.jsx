3bd01a2e045aa9e8e55a7ef143502ddb398720ee
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:49:23 +0530

    update all

--- basic-react/src/components/usestate.jsx
+++ basic-react/src/components/usestate.jsx
@@ -31,13 +31,3 @@ function FavoriteColor() {
 createRoot(document.getElementById('root')).render(
   <FavoriteColor />
 );
-
-import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-
-function MyCar() {
-  const [brand, setBrand] = useState("Ford");
-  const [model, setModel] = useState("Mustang");
-  const [year, setYear] = useState("1964");
-  const [color, setColor] = useState("red");
-
