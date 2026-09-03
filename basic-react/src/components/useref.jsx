43c01d4d4d7a284bfed0b664db7796b27d4ea228
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:34:14 +0530

    update all

--- basic-react/src/components/useref.jsx
+++ basic-react/src/components/useref.jsx
@@ -47,16 +47,5 @@ createRoot(document.getElementById('root')).render(
   <App />
 );
 
-import { useRef, useState, useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-
-function App() {
-  const [inputValue, setInputValue] = useState("");
-  const previousInputValue = useRef("");
-
-  useEffect(() => {
-    previousInputValue.current = inputValue;
-  }, [inputValue]);
-
 
 
