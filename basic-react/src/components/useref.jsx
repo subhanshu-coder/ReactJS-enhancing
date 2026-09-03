5fbc7f18bb0aed8618fc4d55cfe225b4b35dc576
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-03 23:36:09 +0530

    update all

--- basic-react/src/components/useref.jsx
+++ basic-react/src/components/useref.jsx
@@ -1,16 +1,3 @@
-import { useState, useRef, useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-
-function App() {
-  const [inputValue, setInputValue] = useState("");
-  const count = useRef(0);
-
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
-   return (
-    <>
-
 
 
 
