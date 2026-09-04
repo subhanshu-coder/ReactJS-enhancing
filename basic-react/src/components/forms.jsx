15804673b94d3c5477e2f87f2dc94c81e4657d0d
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-04 16:20:46 +0530

    update all

--- basic-react/src/components/forms.jsx
+++ basic-react/src/components/forms.jsx
@@ -1,12 +1,12 @@
-// function MyForm() {
-//   return (
-//     <form>
-//       <label>Enter your name:
-        {/* <input type="text" />
+ function MyForm() {
+  return (
+   <form>
+      <label>Enter your name:
+         <input type="text" />
       </label>
     </form>
   )
-} */}
+} 
 // createRoot(document.getElementById('root')).render(
 //   <MyForm />
 // );
