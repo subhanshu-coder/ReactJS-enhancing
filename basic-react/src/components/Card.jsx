2e439bc54218c454bdad6ff0e385ce420cd19a39
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-04 15:07:34 +0530

    update all

--- basic-react/src/components/Card.jsx
+++ basic-react/src/components/Card.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 function Card(props) {
   return (
-    <div className="child">
-      <img
+    <div 
+    className="child">
+       <img 
         src={props.img}
-        alt=""
-      />
-
+        alt="" 
+    />
+      {props.children}
       <h1>{props.user}</h1>
       <p>Just sense the human behaviour how they belong to dumb society</p>
       <button>Check the profile</button>
