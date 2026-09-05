6ee78135f0d4962a2d9c2f02e866abb8b3a3d6f9
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-06 00:01:13 +0530

    update all

--- basic-react/src/components/Card.jsx
+++ basic-react/src/components/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 function Card(props) {
-  // return (
-  //   <div 
-  //   className="child">
-  //      <img 
-  //       src={props.img}
-  //       alt="" 
-  //   />
-  //     {props.children}
-  //     <h1>{props.user}</h1>
-  //     <p>Just sense the human behaviour how they belong to dumb society</p>
-  //     <button>Check the profile</button>
-  //   </div>
-  // )
-}
+//   // return (
+//   //   <div 
+//   //   className="child">
+//   //      <img 
+//   //       src={props.img}
+//   //       alt="" 
+//   //   />
+//   //     {props.children}
+//   //     <h1>{props.user}</h1>
+//   //     <p>Just sense the human behaviour how they belong to dumb society</p>
+//   //     <button>Check the profile</button>
+//   //   </div>
+//   // )
+// }
 
 export default Card
