8deed66659326350e43cd14cb32f05536ca61cdb
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-05 13:20:23 +0530

    update all

--- basic-react/src/components/useeffect.jsx
+++ basic-react/src/components/useeffect.jsx
@@ -1,4 +1,23 @@
+const App = () => {
 
+const [run , setNum ] =usestate(0)
+ 
+  useEffect(function() {
+    console.log("useEffect is called")
+  })
+
+    return (
+        <div>
+            <h1>num</h1>
+            <button onClick = {() => {
+                setNum(run+1)
+            }
+            } >click me!</button>
+        </div>
+    )
+}
+
+export default App
 
 
 
