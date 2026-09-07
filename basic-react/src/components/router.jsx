64d43052ac615dc642169970e00b4940d6061a6a
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-07 23:42:39 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -7,39 +7,39 @@ function App() {
 }
 
 //example of react router
-// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-// function Home() {
-//   return <h1>Home Page</h1>;
-// }
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+function Home() {
+  return <h1>Home Page</h1>;
+}
 
-// function About() {
-//   return <h1>About Page</h1>;
-// }
+function About() {
+  return <h1>About Page</h1>;
+}
 
-// function Contact() {
-//   return <h1>Contact Page</h1>;
-// }
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       {/* Navigation */}
-//       <nav>
-//         <Link to="/">Home</Link> |{" "}
-//         <Link to="/about">About</Link> |{" "}
-//         <Link to="/contact">Contact</Link>
-//       </nav>
+function App() {
+  return (
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
 
       
-//       {/* Routes */}
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
  
 
 // new one
