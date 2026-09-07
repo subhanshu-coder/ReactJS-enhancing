7c6a3d0381bab94a134bafc722a6dfb105afaa55
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-07 23:41:08 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -75,18 +75,18 @@ function CarProducts() {
   );
 }
 
-// function BikeProducts() {
-//   return (
-//     <div>
-//       <h2>Bikes</h2>
-//       <ul>
-//         <li>Yamaha</li>
-//         <li>Suzuki</li>
-//         <li>Honda</li>
-//       </ul>
-//     </div>
-//   );
-// }
+function BikeProducts() {
+  return (
+    <div>
+      <h2>Bikes</h2>
+      <ul>
+        <li>Yamaha</li>
+        <li>Suzuki</li>
+        <li>Honda</li>
+      </ul>
+    </div>
+  );
+}
 
 // function Contact() {
 //   return <h1>Contact Page</h1>;
