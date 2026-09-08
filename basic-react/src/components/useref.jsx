25e137a205b9bb9f1145708a6b52e695e3e600ff
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-08 23:37:49 +0530

    update all

--- basic-react/src/components/useref.jsx
+++ basic-react/src/components/useref.jsx
@@ -1,19 +1,19 @@
 
-// import React, { useRef } from 'react';
+import React, { useRef } from 'react';
 
-// const UseRefExample = () => {
-//   const inputRef = useRef(null);
+const UseRefExample = () => {
+  const inputRef = useRef(null);
 
-//     const handleClick = () => {
-//     inputRef.current.focus();
-//     };
+    const handleClick = () => {
+    inputRef.current.focus();
+    };
 
-//     return (
-//         <div>
-//             <input ref={inputRef} type="text" placeholder="Type something..." />
-//             <button onClick={handleClick}>Focus Input</button>
-//         </div>
-//     );
-// }
+    return (
+        <div>
+            <input ref={inputRef} type="text" placeholder="Type something..." />
+            <button onClick={handleClick}>Focus Input</button>
+        </div>
+    );
+}
 
 
