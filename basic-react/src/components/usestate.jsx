c21fec72f85f8df5814906a98f978eb905e7cb08
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-08 23:38:14 +0530

    update all

--- basic-react/src/components/usestate.jsx
+++ basic-react/src/components/usestate.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function Counter() {
- const [count, setCount] = useState(0);
-    return(
-<div>
-<h2>you have clicked {count} times</h2>
-<button onClick ={() => {setCount(count +1)}}>Click me</button>
-<button onClick ={() => {setCount(count-1)}}>Click me</button>
-</div>
-    )
-}
+// function Counter() {
+//  const [count, setCount] = useState(0);
+//     return(
+// <div>
+// <h2>you have clicked {count} times</h2>
+// <button onClick ={() => {setCount(count +1)}}>Click me</button>
+// <button onClick ={() => {setCount(count-1)}}>Click me</button>
+// </div>
+//     )
+// }
 
-export default Counter;
+// export default Counter;
