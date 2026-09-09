03ee527d2849719db990a9abe8d0c411f0cd0fe1
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-09 23:55:11 +0530

    update all

--- basic-react/src/components/conditional.jsx
+++ basic-react/src/components/conditional.jsx
@@ -4,17 +4,17 @@
 // Example:
 // Now, we'll create another component that chooses which component to render based on a condition:
 
-// function Goal(props) {
-//   const isGoal = props.isGoal;
-//    if (isGoal) {
-//     return <MadeGoal/>;
-//   }
-//     return <MissedGoal/>;
-// }
+function Goal(props) {
+  const isGoal = props.isGoal;
+   if (isGoal) {
+    return <MadeGoal/>;
+  }
+    return <MissedGoal/>;
+}
 
-// createRoot(document.getElementById('root')).render(
-//   <Goal isGoal={false} />
-// );
+createRoot(document.getElementById('root')).render(
+  <Goal isGoal={false} />
+);
 
 // Try changing the isGoal attribute to true:
 // Example:
