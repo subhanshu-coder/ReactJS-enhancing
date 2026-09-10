38e70a1532b9a5f31a0cc1d6253bad26d9dfc009
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-11 00:00:02 +0530

    update all

--- basic-react/src/components/props.jsx
+++ basic-react/src/components/props.jsx
@@ -29,38 +29,38 @@ function Car(props) {
 // // variables has to be sent inside curly brackets
 // // let x = "Ford";
 
-createRoot(document.getElementById('root')).render(
-  <Car brand={x} />
-);
+// createRoot(document.getElementById('root')).render(
+//   <Car brand={x} />
+// );
 
-// // Objects and Arrays has to be sent inside curly brackets:
-let x = [1964, 1965, 1966];
-let y = {name: "Ford", model: "Mustang"};
-
-createRoot(document.getElementById('root')).render(
-  <Car years={x} carinfo={y} />
-);
-
-// // Use the dot notation to access object properties:
-// function Car(props) {
-//   return (
-//     <>
-//       <h2>My {props.carinfo.name} {props.carinfo.model}!</h2>
-//       <p>It is {props.carinfo.color} and it is from {props.carinfo.year}!</p>
-//     </>
-//   );
-// }
-
-// const carInfo = {
-//   name: "Ford",
-//   model: "Mustang",
-//   color: "red",
-//   year: 1969
-// };
+// // // Objects and Arrays has to be sent inside curly brackets:
+// let x = [1964, 1965, 1966];
+// let y = {name: "Ford", model: "Mustang"};
 
 // createRoot(document.getElementById('root')).render(
-//   <Car carinfo={carInfo} />
+//   <Car years={x} carinfo={y} />
 // );
+
+// // Use the dot notation to access object properties:
+function Car(props) {
+  return (
+    <>
+      <h2>My {props.carinfo.name} {props.carinfo.model}!</h2>
+      <p>It is {props.carinfo.color} and it is from {props.carinfo.year}!</p>
+    </>
+  );
+}
+
+const carInfo = {
+  name: "Ford",
+  model: "Mustang",
+  color: "red",
+  year: 1969
+};
+
+createRoot(document.getElementById('root')).render(
+  <Car carinfo={carInfo} />
+);
 
 // // Use the indexes to access array properties:
 
