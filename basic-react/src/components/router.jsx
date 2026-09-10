f905cb6ac31690832ddf9abf2d0983ee3f3387be
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-10 23:55:58 +0530

    update all

--- basic-react/src/components/router.jsx
+++ basic-react/src/components/router.jsx
@@ -1,92 +1,92 @@
-function App() {
-  return (
-    <BrowserRouter>
-      {/* Your app content */}
-    </BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       {/* Your app content */}
+//     </BrowserRouter>
+//   );
+// }
 
-//example of react router
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-function Home() {
-  return <h1>Home Page</h1>;
-}
+// //example of react router
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// function Home() {
+//   return <h1>Home Page</h1>;
+// }
 
-function About() {
-  return <h1>About Page</h1>;
-}
+// function About() {
+//   return <h1>About Page</h1>;
+// }
 
-function Contact() {
-  return <h1>Contact Page</h1>;
-}
+// function Contact() {
+//   return <h1>Contact Page</h1>;
+// }
 
-function App() {
-  return (
-    <BrowserRouter>
-      {/* Navigation */}
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/contact">Contact</Link>
-      </nav>
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       {/* Navigation */}
+//       <nav>
+//         <Link to="/">Home</Link> |{" "}
+//         <Link to="/about">About</Link> |{" "}
+//         <Link to="/contact">Contact</Link>
+//       </nav>
 
       
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+//       {/* Routes */}
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
  
 
 // new one
 
-// import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
-// function Home() {
-//   return <h1>Home Page</h1>;
-// }
-// function Products() {
-//   return (
-//     <div>
-//       <h1>Products Page</h1>
-//       <nav style={{ marginBottom: '20px' }}>
-//         <Link to="/products/car">Cars</Link> |{" "}
-//         <Link to="/products/bike">Bikes</Link>
-//       </nav> 
-//       <Outlet /> 
-//     </div>
-//   );
-// }
+function Home() {
+  return <h1>Home Page</h1>;
+}
+function Products() {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <nav style={{ marginBottom: '20px' }}>
+        <Link to="/products/car">Cars</Link> |{" "}
+        <Link to="/products/bike">Bikes</Link>
+      </nav> 
+      <Outlet /> 
+    </div>
+  );
+}
 
-// function CarProducts() {
-//   return (
-//     <div>
-//       <h2>Cars</h2>
-//       <ul>
-//         <li>Audi</li>
-//         <li>BMW</li>
-//         <li>Volvo</li>
-//       </ul>
-//     </div>
-//   );
-// }
+function CarProducts() {
+  return (
+    <div>
+      <h2>Cars</h2>
+      <ul>
+        <li>Audi</li>
+        <li>BMW</li>
+        <li>Volvo</li>
+      </ul>
+    </div>
+  );
+}
 
-// function BikeProducts() {
-//   return (
-//     <div>
-//       <h2>Bikes</h2>
-//       <ul>
-//         <li>Yamaha</li>
-//         <li>Suzuki</li>
-//         <li>Honda</li>
-//       </ul>
-//     </div>
-//   );
-// }
+function BikeProducts() {
+  return (
+    <div>
+      <h2>Bikes</h2>
+      <ul>
+        <li>Yamaha</li>
+        <li>Suzuki</li>
+        <li>Honda</li>
+      </ul>
+    </div>
+  );
+}
 
 function Contact() {
   return <h1>Contact Page</h1>;
