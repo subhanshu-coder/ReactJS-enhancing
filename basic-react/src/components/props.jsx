f8ea9c3f3255ac32fc4ebe1b47c73a16468d3ea5
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-10 23:58:32 +0530

    update all

--- basic-react/src/components/props.jsx
+++ basic-react/src/components/props.jsx
@@ -64,17 +64,17 @@ createRoot(document.getElementById('root')).render(
 
 // // Use the indexes to access array properties:
 
-// function Car(props) {
-//   return (
-//     <h2>My car is a {props.carinfo[0]} {props.carinfo[1]}!</h2>
-//   );
-// }
+function Car(props) {
+  return (
+    <h2>My car is a {props.carinfo[0]} {props.carinfo[1]}!</h2>
+  );
+}
 
-// const carInfoArray = ["Ford", "Mustang"];
+const carInfoArray = ["Ford", "Mustang"];
 
-// createRoot(document.getElementById('root')).render(
-//   <Car carinfo={carInfoArray} />
-// );
+createRoot(document.getElementById('root')).render(
+  <Car carinfo={carInfoArray} />
+);
 // import PropTypes from 'prop-types'
 // function Student(props) {
 //   return (
