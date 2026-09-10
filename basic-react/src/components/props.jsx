f5b10ad5426dbc3e7a01c5c8e1deb35e575d8f96
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-11 00:01:03 +0530

    update all

--- basic-react/src/components/props.jsx
+++ basic-react/src/components/props.jsx
@@ -7,24 +7,24 @@ function Car(props) {
 }
 // export default Car
 
-// // // you can use myobj instead of props in the component
-// function Car(myobj) {
-//   return (
-//     <h2>I am a {myobj.brand}!</h2>
-//   );
-// }
+// // you can use myobj instead of props in the component
+function Car(myobj) {
+  return (
+    <h2>I am a {myobj.brand}!</h2>
+  );
+}
 
-// // // send multiple properties to the car component
-// createRoot(document.getElementById('root')).render(
-//   <Car brand="Ford" model="Mustang" color="red" />
-// );
+// // send multiple properties to the car component
+createRoot(document.getElementById('root')).render(
+  <Car brand="Ford" model="Mustang" color="red" />
+);
 
-// // // use the property values in the car component
-// function Car(props) {
-//   return (
-//     <h2>I am a {props.color} {props.brand} {props.model}!</h2>
-//   );
-// }
+// // use the property values in the car component
+function Car(props) {
+  return (
+    <h2>I am a {props.color} {props.brand} {props.model}!</h2>
+  );
+}
 
 // // variables has to be sent inside curly brackets
 // // let x = "Ford";
