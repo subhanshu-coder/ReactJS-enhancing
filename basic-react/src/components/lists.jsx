70facfa5a3c7c032cb5b57f2c1d3cf7d62753231
Subhanshu pal <subhanshupal7@gmail.com>
2026-09-11 22:56:39 +0530

    update all

--- basic-react/src/components/lists.jsx
+++ basic-react/src/components/lists.jsx
@@ -1,18 +1,18 @@
- function MyCars() {
-   const cars = ['Ford', 'BMW', 'Audi'];
-   return (
-    <>
-      <h1>My Cars:</h1>
-      <ul>
-        {cars.map((car) => <li>I am a { car }</li>)}
-      </ul>
-    </>
-      );
-}
+//  function MyCars() {
+//    const cars = ['Ford', 'BMW', 'Audi'];
+//    return (
+//     <>
+//       <h1>My Cars:</h1>
+//       <ul>
+//         {cars.map((car) => <li>I am a { car }</li>)}
+//       </ul>
+//     </>
+//       );
+// }
 
-createRoot(document.getElementById('root')).render(
-  <MyCars />
-);
+// createRoot(document.getElementById('root')).render(
+//   <MyCars />
+// );
 
 function MyCars() {
   const cars = [
